26a42965c5f98695a332bc7092ce1d297afce504
Yeji-Jang1210 <jyg8727@gmail.com>
2024-05-25 14:48:53 +0900

    Initial Commit

---
+++ TravelMagazine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelMagazine
+//
+//  Created by 장예지 on 5/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
